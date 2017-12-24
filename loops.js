a967963da3ee170f780b6b4b1a976a56463074ae
Matt Redmon <matt.redmon@live.com>
2017-12-24 02:17:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,6 +26,8 @@ function maybeTrue() {
 }
 
 function doWhileLoop(array) {
-  array.pop 
-} while (array.length > 0 && maybeTrue());
-return array
+  do {
+    array.pop 
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
